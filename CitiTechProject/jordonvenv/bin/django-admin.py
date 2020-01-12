2e3749c7b77e42c10e33622ba590237c713ae7ee
Jordon Douglas <jdouglas2015@gmail.com>
2020-01-12 20:56:39 +0000

    added ip address

---
+++ CitiTechProject/jordonvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/var/www/citi_tech/passion_project_Redo_2019/CitiTechProject/jordonvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ CitiTechProject/static/CitiTechApp/js/home.js
@@ -0,0 +1,5 @@
+
+function showEvents()
+{
+
+}
